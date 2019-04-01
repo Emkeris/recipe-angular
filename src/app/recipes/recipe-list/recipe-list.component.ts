87c3dc6c7ca40ab398ae640640fda96b760d4646
emkeris <emkeris@gmail.com>
2019-04-01 10:53:36 +0100

    all basic components added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    // tslint:disable-next-line:max-line-length
+    new Recipe('Test Recipie', 'A simple dummy Test Recipie', 'https://get.pxhere.com/photo/cafe-tea-dish-food-produce-vegetable-meat-lunch-pork-cuisine-delicious-steak-pork-chop-dinner-new-zealand-western-ribs-grilling-spare-ribs-fox-glacier-pork-ribs-fried-food-animal-source-foods-556368.jpg'),
+    // tslint:disable-next-line:max-line-length
+    new Recipe('Test Recipie 2', 'A simple dummy Test Recipie 2', 'https://get.pxhere.com/photo/cafe-tea-dish-food-produce-vegetable-meat-lunch-pork-cuisine-delicious-steak-pork-chop-dinner-new-zealand-western-ribs-grilling-spare-ribs-fox-glacier-pork-ribs-fried-food-animal-source-foods-556368.jpg')
+
+  ];
 
   constructor() { }
 
